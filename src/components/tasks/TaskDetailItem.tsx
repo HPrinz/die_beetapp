0cd1c1f4b79e9aaab632469f78b1db17e29c1ce6
Hanna Prinz <hanna_prinz@yahoo.de>
2018-07-21 23:37:20 +0200

    share

--- src/components/tasks/TaskDetailItem.tsx
+++ src/components/tasks/TaskDetailItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, Image} from "react-native";
+import { StyleSheet, Text, Image, Share, ShareContent, ShareOptions} from "react-native";
 import { Card, Button } from "react-native-elements";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
@@ -8,7 +8,7 @@ import { RouteComponentProps, withRouter, Link } from "react-router-native";
 import { RootState } from "../../reducers";
 import { Bed, Task, cropTypes, Crop } from "../../reducers/garden";
 import { selectTask, OtherActionResponse, setBedForTask, markTaskResolved } from "../../actions";
-import { taskTypes } from "../../data/tasks";
+import { taskTypes, TaskType } from "../../data/tasks";
 
 type OwnProps = {};
 
@@ -24,7 +24,11 @@ type DispatchToPropsType = {
   onMarkTaskResolved: (taskId : string) => void;
 };
 
-type State = {};
+type State = {
+  bed : Bed | undefined,
+  crop : Crop | undefined,
+  taskType: TaskType;
+};
 
 export type Props = RouteComponentProps<{}> &
   OwnProps &
@@ -35,20 +39,41 @@ export type Props = RouteComponentProps<{}> &
 class TaskDetailItem extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
+    const { task} = this.props;
+    this.state = {
+      bed : task.bedId && this.props.beds.find(bed => bed.id === task.bedId) ? (this.props.beds.find(bed => bed.id === task.bedId) as Bed) : undefined,
+      crop : task.cropId && cropTypes.find(crop => crop.id === (task.cropId as string)) ? (cropTypes.find(crop => crop.id === (task.cropId as string)) as Crop) : undefined,
+      taskType: taskTypes[task.taskType]
+    };
+    this._shareTask = this._shareTask.bind(this);
+  }
+
+  private _shareTask(){
+    const { taskType, bed, crop } = this.state;
+    const bedText = !bed ? '' : ' im ' + bed.name;   
+    const cropText = !crop ? '' : ' bei den ' + crop.name + 'n';   
+    const shareContent = {
+      message: 'Kannst du vielleicht das ' + taskType.name + bedText + cropText + ' übernehmen?',
+      url: 'http://beetapp.de',
+      title: 'die beetapp Aufgabe teilen'
+    } as ShareContent
+    const shareOpts = {
+      // Android only:
+      dialogTitle: 'die beetapp',} as ShareOptions
+    Share.share(shareContent, shareOpts);
   }
 
   render() {
-    const { task } = this.props;
-    if (!task) {
-      return <Text>NO ITEM {this.props.selectedId}</Text>;
-    }
+    const { task} = this.props;
+    const { taskType, bed, crop } = this.state;
     return (
-      <Card title={taskTypes[task.taskType].name} image={taskTypes[task.taskType].icon} imageStyle={{height: 20, width: 20, flex: 1, alignSelf: 'center'}} >
-        <Image style={{flex: 1, width: 250, height: 200}} source={taskTypes[task.taskType].image} resizeMode="contain" />
-        <Text>{taskTypes[task.taskType].description}</Text>
-        <Text>Beet: {task.bedId && this.props.beds.find(bed => bed.id === task.bedId) ? (this.props.beds.find(bed => bed.id === task.bedId) as Bed).name : ''}</Text>
-        <Text>Kultur: {task.cropId && cropTypes.find(crop => crop.id === (task.cropId as string)) ? (cropTypes.find(crop => crop.id === (task.cropId as string)) as Crop).name : ''}</Text>
+      <Card title={taskType.name} image={taskType.icon} imageStyle={{height: 20, width: 20, flex: 1, alignSelf: 'center'}} >
+        <Image style={{flex: 1, width: 250, height: 200}} source={taskType.image} resizeMode="contain" />
+        <Text>{ bed ? 'Beet: ' + bed.name : '' }</Text>
+        <Text>{ crop ? 'Kultur: ' + crop.name : ''}</Text>
+        <Text>{taskType.description}</Text>
         <Button onPress={() => this.props.onMarkTaskResolved(task.id)} title='erledigt'></Button>
+        <Button onPress={() => this._shareTask()} title='teilen'></Button>
         <Link to="/" component={Button} onPress={() => this.props.onBack()} title='zurück' />
       </Card>
     );
@@ -58,10 +83,8 @@ class TaskDetailItem extends React.Component<Props, State> {
 function mapStateToProps(state: RootState): StateToPropsType {
   return {
     beds: state.garden.setup.beds,
-    task: state.garden.tasks.find(
-      x => x.id === state.garden.selectedTaskId
-    ) as Task,
-    selectedId: state.garden.selectedTaskId
+    task: state.garden.tasks.find( x => x.id === state.garden.selectedTaskId) as Task,
+    selectedId: state.garden.selectedTaskId,
   };
 }
 
@@ -83,8 +106,3 @@ export default withRouter(
   )(TaskDetailItem)
 );
 
-const styles = StyleSheet.create({
-  root: {
-    flex: 1
-  }
-});
